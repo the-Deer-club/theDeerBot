1d9dc64bd39a03cd4a548a38d3dacc2c28979b02
Lamtailoi2 <lamtailoi11141@gmail.com>
2024-07-28 05:10:26 +0700

    [CD01][LoiLT] feat: add slash command

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js'
+import { Client, Events, GatewayIntentBits, REST } from 'discord.js'
+import { textingEvents } from './events/texting'
+import slashRegister from './commands/slashRegister'
+import { pingCommand } from './commands/commands'
 import 'dotenv/config'
 
 const TOKEN = process.env.DISCORD_BOT_TOKEN
@@ -8,11 +11,20 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent,
   ],
 })
 
-client.once(Events.ClientReady, clientReady => {
+client.on(Events.MessageCreate, async message => {
+  if (message.author.bot) return
+  await textingEvents(message)
+})
+
+client.on('interactionCreate', interaction => pingCommand(interaction))
+
+client.once(Events.ClientReady, async clientReady => {
   console.log(`client listening at ${client.user?.displayName}`)
+  await slashRegister(client)
 })
 
 client.login(TOKEN)
