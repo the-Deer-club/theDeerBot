612033960fbec9a5dbb69c2ecc411e4918262c2f
lamtailoi2 <lamtailoi11141@gmail.com>
2024-08-02 02:54:52 +0700

    [CD01][LoiLT] fix: fix handleCommand

--- src/events/interactionCreate/handleCommands.ts
+++ src/events/interactionCreate/handleCommands.ts
@@ -1,9 +1,10 @@
-import type { Client, CommandInteraction } from 'discord.js'
+import type { Client, CommandInteraction, Interaction } from 'discord.js'
 import type { CustomCommand } from '../../utils/types'
+import { InteractionType } from 'discord.js'
 import getLocalCommands from '../../utils/getLocalCommands'
 export default async (
   client: Client,
-  interaction: CommandInteraction,
+  interaction: Interaction,
 ): Promise<any> => {
   const guildId = client.guilds.cache.first()?.id
   console.log(guildId)
@@ -12,18 +13,21 @@ export default async (
     return
   }
   const localCommands = await getLocalCommands()
+
+  if (interaction.type !== InteractionType.ApplicationCommand) {
+    console.log('Interaction is not a command')
+    return
+  }
   if (!Array.isArray(localCommands)) {
     throw new Error('Local commands not found')
   }
   const cmdArr = Array.from(localCommands)
-
-  const executedCommand: CustomCommand = cmdArr.find(
+  const command = cmdArr.find(
     (cmd: CustomCommand) => cmd.name === interaction.commandName,
   )
-  if (!executedCommand) {
-    console.log('Command not found');
-    
+  if (!command) {
+    console.log('Command not found')
+    return
   }
-  await executedCommand?.execute(client, interaction)
-
+  command.execute(client, interaction as CommandInteraction)
 }
--- src/handlers/eventHanlder.ts
+++ src/handlers/eventHanlder.ts
@@ -1,32 +1,29 @@
 import path from 'path'
 import { getAllFiles } from '../utils/getAllFiles'
-import type { Client, Message } from 'discord.js'
+import type { Client } from 'discord.js'
 
-export const eventHandler = (client: Client, msg?: Message): void => {
+export const eventHandler = (client: Client): void => {
   const eventFolders = getAllFiles(path.join(__dirname, '..', 'events'), true)
-  
+
   for (const eventFolder of eventFolders) {
     const eventFiles = getAllFiles(eventFolder)
     eventFiles.sort((a, b) => a.localeCompare(b))
 
     const eventName = eventFolder.replace(/\\/g, '/').split('/').pop()
-    
+
     if (!eventName) {
       console.warn(`Skipping folder without a valid event name: ${eventFolder}`)
       continue
     }
-    
-    
-    client.on('ready', args => {
+
+    client.on(eventName, args => {
       ;(async () => {
         for (const eventFile of eventFiles) {
-          console.log(`Loading event: ${eventFile}`);
-          
+          console.log(`Loading event: ${eventFile}`)
           const eventModule = await import(eventFile)
           const eventFunction = eventModule.default || eventModule
           await eventFunction(client, args)
         }
-        
       })().catch(err => {
         console.log(err)
       })
