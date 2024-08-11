1d783f5505be4fea8483a1b4650f1790a3ec4644
Lâm Tài Lợi <143779007+lamtailoi2@users.noreply.github.com>
2024-08-11 17:52:45 +0700

    Update src/commands/misc/ping.ts
    Co-authored-by: Trần Quang Sáng <76774572+SangTran-127@users.noreply.github.com>

--- src/commands/misc/ping.ts
+++ src/commands/misc/ping.ts
@@ -9,9 +9,7 @@ const pingCommand: CustomCommand = {
     await interaction.deferReply()
     const reply = await interaction.editReply('Pong!')
     const ping = reply.createdTimestamp - interaction.createdTimestamp
-    await interaction.editReply(`Pong! \`${ping}ms\``).catch(err => {
-      console.log(err)
-    })
+    await interaction.editReply(`Pong! \`${ping}ms\``).catch(console.error)
   },
 }
 
