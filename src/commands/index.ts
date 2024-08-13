911430e22fc2d8d9a867120b71fb46910ee38f3c
lamtailoi2 <lamtailoi11141@gmail.com>
2024-08-14 03:42:49 +0700

    feat: add kick command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,8 +1,9 @@
 import playCommand from './misc/play'
 import pingCommand from './misc/ping'
 import skipCommand from './misc/skip'
+import kickCommand from './misc/kick'
 import type { CustomCommand } from '../utils/types'
 
-const commandList: CustomCommand[] = [playCommand, pingCommand, skipCommand]
+const commandList: CustomCommand[] = [playCommand, pingCommand, skipCommand, kickCommand]
 
 export default commandList
