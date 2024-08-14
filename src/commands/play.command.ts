caf0569e174a307dcc3967d862e5ad8456e27a92
lamtailoi2 <lamtailoi11141@gmail.com>
2024-08-14 17:38:49 +0700

    fix: rename command file

---
+++ src/commands/play.command.ts
@@ -0,0 +1,76 @@
+import type { CommandInteraction, GuildMember } from 'discord.js'
+import type { CustomCommand } from '../utils/types'
+import type { CustomClient } from '../class/CustomClient'
+import { EmbedBuilder } from 'discord.js'
+import { QueryType } from 'discord-player'
+import { YoutubeiExtractor } from 'discord-player-youtubei'
+
+const playCommand: CustomCommand = {
+  name: 'play',
+  description: 'Play a song!',
+  options: [
+    {
+      name: 'song',
+      type: 3,
+      description: 'Song to play',
+      required: true,
+    },
+  ],
+  execute: async (client: CustomClient, interaction: CommandInteraction) => {
+    try {
+      if (!interaction.isChatInputCommand()) return
+
+      const song = interaction.options.getString('song')
+      if (!song) {
+        await interaction.reply('Vui lòng nhập tên bài hát!')
+        return
+      }
+
+      const member = interaction.member as GuildMember
+      if (!member.voice.channel) {
+        await interaction.reply('Vào phòng đi tui hát cho nghe, :3')
+        return
+      }
+
+      const clientPlayer = client.player
+      await clientPlayer.extractors.register(YoutubeiExtractor, {})
+
+      const queue = interaction.guild
+        ? clientPlayer.nodes.create(interaction.guild)
+        : null
+
+      if (queue) {
+        if (!queue.connection) {
+          await queue.connect(member.voice.channel)
+        }
+        const searchResult = await clientPlayer.search(song, {
+          requestedBy: interaction.user,
+          searchEngine: QueryType.AUTO,
+        })
+
+        if (!searchResult.tracks.length) {
+          await interaction.reply('Không tìm thấy bài hát!')
+          return
+        }
+
+        const track = searchResult.tracks[0]
+        queue?.addTrack(track)
+
+        await queue.play(track)
+
+        const embed = new EmbedBuilder()
+          .setDescription(`Added **[${track.title}]** to the queue`)
+          .setThumbnail(track.thumbnail)
+          .setFooter({ text: `Duration: ${track.duration}` })
+        await interaction.reply({ embeds: [embed] })
+        return
+      }
+      await interaction.reply('Thêm bài hát thất bại!')
+    } catch (error) {
+      console.error('Error in playCommand:', error)
+      await interaction.reply('Vấn đề kĩ thuật, mong bạn thông cảm :3')
+    }
+  },
+}
+
+export default playCommand
---
+++ src/commands/skip.command.ts
@@ -0,0 +1,40 @@
+import type { CommandInteraction } from 'discord.js'
+import type { CustomCommand } from '../utils/types'
+import type { CustomClient } from '../class/CustomClient'
+import { EmbedBuilder } from 'discord.js'
+
+const skipCommand: CustomCommand = {
+  name: 'skip',
+  description: 'Skip current song',
+
+  execute: async (client: CustomClient, interaction: CommandInteraction) => {
+    try {
+      const clientPlayer = client.player
+      const queue = clientPlayer.nodes.get(interaction.guildId ?? '')
+      if (!queue) {
+        await interaction.reply('No song is playing!')
+        return
+      }
+
+      const currentSong = queue.currentTrack
+      if (!currentSong) {
+        await interaction.reply('No song is currently playing!')
+        return
+      }
+      queue.node.skip()
+      queue.removeTrack(currentSong)
+
+      await interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setDescription(`Skipped **${currentSong.title}`)
+            .setThumbnail(currentSong.thumbnail),
+        ],
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  },
+}
+
+export default skipCommand
